6f78055ffba2e362f3800d2e839d5f7040346f2d
waw <81994421+ambr0sial@users.noreply.github.com>
2025-01-16 14:27:52 +0100

    16/01 <33

---
+++ sources.js
@@ -0,0 +1,38 @@
+const availableSources = [
+    {
+        id: 'multiembed',
+        name: 'Multiembed',
+        isFrench: false,
+        urls: {
+            movie: 'https://multiembed.mov/?video_id={id}&tmdb=1',
+            tv: 'https://multiembed.mov/?video_id={id}&tmdb=1&s={season}&e={episode}'
+        }
+    },
+    {
+        id: 'vidbinge',
+        name: 'Vidbinge (4K)',
+        isFrench: false,
+        urls: {
+            movie: 'https://vidbinge.dev/embed/movie/{id}',
+            tv: 'https://vidbinge.dev/embed/tv/{id}/{season}/{episode}'
+        }
+    },
+    {
+        id: 'frembed',
+        name: 'Frembed',
+        isFrench: true,
+        urls: {
+            movie: 'https://frembed.lol/api/film.php?id={id}',
+            tv: 'https://frembed.lol/api/serie.php?id={id}&sa={season}&epi={episode}'
+        }
+    },
+    {
+        id: 'moviesapi',
+        name: 'MoviesAPI',
+        isFrench: false,
+        urls: {
+            movie: 'https://moviesapi.club/movie/{id}',
+            tv: 'https://moviesapi.club/tv/{id}-{season}-{episode}'
+        }
+    }
+]; 
